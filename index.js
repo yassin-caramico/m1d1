9544ee2d21bd6579a08f31f45c2a7142d24e14c8
lucaperullo <lucaperullo@outlook.it>
2024-01-22 21:18:54 +0100

    exercises

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// ge
+/* EXERCISE 1
+Write the code to execute a SUM between the number 12 and 20
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 2
+Create a variable named X containing the number 12
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 3
+Create a variable named name containing the string John Doe
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 4
+Execute a DIFFERENCE between the number 12 and the variable X, which stores the value 12
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 5
+Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
+Verify that name1 is different from name2. 
+Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2)
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 6
+Create the variable X (value less than 10). Write the code to print the literal value of the given number (ex.: 1 => one, 5 => five)
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 7 (EXTRA)
+Insert a value in a variable based on the result of a ternary if
+*/
+
+/* WRITE YOUR CODE HERE */
