6f2cda05a9c1290f08db66db92c17902f842c236
Yassin Caramico <Ycaramico1202@gmail.com>
2024-01-23 00:12:14 +0100

    dimmi se è giusto il 6

--- index.js
+++ index.js
@@ -33,6 +33,41 @@ Verify then, that name1 and name2 are equals if both lowercase (without changing
 /* EXERCISE 6
 Create the variable X (value less than 10). Write the code to print the literal value of the given number (ex.: 1 => one, 5 => five)
 */
+let x = 8
+function StampaValoreLetterale(8) {
+    switch (numero) {
+        case 1:
+            console.log("uno");
+            break;
+        case 2:
+            console.log("due");
+            break;
+        case 3:
+            console.log("tre");
+            break;
+        case 4:
+            console.log("quattro");
+            break;
+        case 5:
+            console.log("cinque");
+            break;
+        case 6:
+            console.log("sei");
+            break;
+        case 7:
+            console.log("sette");
+            break;
+        case 8:
+            console.log("otto");
+            break;
+        case 9:
+            console.log("nove");
+            break;
+        default:
+            console.log("Il numero non ha una rappresentazione letterale definita");
+    }
+}
+StampaValoreLetterale(8);
 
 /* WRITE YOUR CODE HERE */
 
